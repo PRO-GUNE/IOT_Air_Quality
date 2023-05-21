0ef3d87f623912ea2b5e5a27f9e442bb2ef3e653
PRO-GUNE <chathuragunasekera@gmail.com>
2023-05-21 20:34:59 +0530

    Calibrated sensor dials Frequency setting - tick retain

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,14 @@
 #define SDA 21 // SDA = GPIO21
 #define SCL 22 // SCL = GPIO22
 Adafruit_SGP30 sgp;
+String SGP30payload; // String to hold SGP30 sensor data
 
 // DHT sensor setup
 #define DHTPIN 4          // DHT22 signal pin is connected to GPIO 4
 #define DHTTYPE DHT22     // DHT22 (AM2302)
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
+float h, t;               // Variables to hold sensor readings
+String DHTpayload;        // String to hold DHT sensor data
 
 // Replace with your network credentials
 const char *ssid = "AndroidAPBD06";
@@ -23,8 +26,12 @@ const char *password = "1234567890";
 const char *mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 
+// Publish topics
 const char *mqtt_topic_DHT = "DATA/ESP32/DHT22";
 const char *mqtt_topic_SGP30 = "DATA/ESP32/SGP30";
+
+// Subscribe topics
+const char *mqtt_topic_frequency = "CONTROL/ESP32/FREQUENCY";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -33,11 +40,113 @@ long last_time = millis(), now, frequency = 10;
 
 void callback(char *topic, byte *payload, unsigned int length);
 void measure();
-void reconnect();
+void MQTTConnect();
+void WiFiConnect();
+void SGP30Init();
+void DHTInit();
 
 void setup()
 {
   Serial.begin(9600);
+
+  // Initialize sensors
+  SGP30Init();
+  DHTInit();
+
+  // Wait for few seconds to initialize the sensors
+  delay(5000);
+}
+
+void loop()
+{
+  // Read the measurements from the sensors
+  measure();
+
+  // MQTT client loop
+  if (client.connected())
+  {
+    client.loop();
+    // Publish data to MQTT broker
+    client.publish(mqtt_topic_DHT, DHTpayload.c_str());
+    client.publish(mqtt_topic_SGP30, SGP30payload.c_str());
+    client.subscribe(mqtt_topic_frequency);
+  }
+  else
+  {
+    // Connect to WiFi network
+    WiFiConnect();
+
+    // Connect to MQTT broker
+    MQTTConnect();
+  }
+}
+
+void DHTInit()
+{
+  // Initialize DHT sensor
+  dht.begin();
+}
+void measureDHT()
+{
+  // Read temperature and humidity data
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+
+  // Check if any reading failed
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Failed to read data from DHT22 sensor");
+    return;
+  }
+  DHTpayload = "temperature: " + String(t) + ", humidity: " + String(h);
+}
+void debugPrintDHT()
+{
+  // Print temperature and humidity values to Serial Monitor
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+}
+
+void SGP30Init()
+{
+  // Initialize SGP30 sensor
+  Wire.begin(SDA, SCL);
+
+  while (!sgp.begin())
+  {
+    Serial.println("Sensor not found :(");
+  }
+
+  Serial.println("SGP30 sensor found");
+}
+void measureSGP30()
+{
+  // Read data from SGP30
+  if (!sgp.IAQmeasure())
+  {
+    Serial.println("Measurement failed");
+    return;
+  }
+  SGP30payload = "eCO2: " + String(sgp.eCO2) + " ppm, TVOC: " + String(sgp.TVOC) + " ppb";
+}
+void debugPrintSGP30()
+{
+  // Print SGP30 values to Serial Monitor
+  Serial.print("eCO2: ");
+  Serial.print(sgp.eCO2);
+  Serial.print(" ppm\tTVOC: ");
+  Serial.print(sgp.TVOC);
+  Serial.println(" ppb");
+}
+
+void WiFiConnect()
+{
+  // Check if the WiFi is already connected
+  if (WiFi.status() == WL_CONNECTED)
+    return;
 
   // Connect to WiFi network
   Serial.println();
@@ -51,66 +160,15 @@ void setup()
   }
   Serial.println("");
   Serial.println("WiFi connected");
+}
 
-  // Connect to MQTT broker
+void MQTTConnect()
+{
+  // Set the server and port
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
-  // Connect to MQTT broker
-  while (!client.connected())
-  {
-    if (client.connect("ESP32Client"))
-    {
-      Serial.println("Connected to MQTT broker");
-    }
-    else
-    {
-      Serial.print("MQTT connection failed, error code: ");
-      Serial.println(client.state());
-      delay(2000);
-    }
-  }
-
-  // Initialize SGP30 sensor
-  Wire.begin(SDA, SCL);
-
-  while (!sgp.begin())
-  {
-    Serial.println("Sensor not found :(");
-  }
-
-  Serial.println("SGP30 sensor found");
-
-  // Initialize DHT sensor
-  dht.begin();
-}
-
-void loop()
-{
-  // MQTT client loop
-  if (client.connected())
-  {
-    client.loop();
-    Serial.println("MQTT client loop");
-
-    // Wait a few seconds between measurements
-    now = millis();
-
-    if (now - last_time > frequency)
-    {
-      last_time = now;
-      measure();
-    }
-  }
-  else
-  {
-    reconnect();
-  }
-}
-
-void reconnect()
-{
-  // reconnect the client
+  // Loop until we're reconnected
   while (!client.connected())
   {
     Serial.println("Connecting to MQTT broker...");
@@ -129,52 +187,32 @@ void reconnect()
 
 void measure()
 {
-  // Read temperature and humidity data
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  // Check if any reading failed
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("Failed to read data from DHT22 sensor");
+  // Get current time
+  now = millis();
+  if (now - last_time <= frequency)
     return;
-  }
 
-  // Print temperature and humidity values
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print("°C  Humidity: ");
-  Serial.print(h);
-  Serial.println("%");
+  // Update the last time measurement
+  last_time = now;
+
+  // Read data from DHT22
+  measureDHT();
 
   // Read data from SGP30
-  if (!sgp.IAQmeasure())
-  {
-    Serial.println("Measurement failed");
-    return;
-  }
-
-  Serial.print("eCO2: ");
-  Serial.print(sgp.eCO2);
-  Serial.print(" ppm\tTVOC: ");
-  Serial.print(sgp.TVOC);
-  Serial.println(" ppb");
-
-  // Publish temperature and humidity values to MQTT broker
-  String payload = "temperature: " + String(t) + ", humidity: " + String(h);
-  client.publish(mqtt_topic_DHT, payload.c_str());
-
-  // Publish eCO2 and TVOC values to MQTT broker
-  payload = "eCO2: " + String(sgp.eCO2) + ", TVOC: " + String(sgp.TVOC);
-  client.publish(mqtt_topic_SGP30, payload.c_str());
+  measureSGP30();
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
   Serial.print("Callback - ");
   Serial.print("Message:");
-  for (int i = 0; i < length; i++)
+
+  // Update the frequency after checking the topic
+  if (strcmp(topic, mqtt_topic_frequency) == 0)
   {
-    Serial.print((char)payload[i]);
+    // set the frequency in minutes
+    frequency = atoi((char *)payload) * 60000;
+    Serial.print("Frequency: ");
+    Serial.println(frequency);
   }
 }
